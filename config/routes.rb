c37504abab1ffee53d6d8ddd9655dd6cd0d20e09
Lynne Kostakis <lkostaki@chicagobooth.edu>
2017-08-10 00:32:59 +0000

    user_list

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,14 @@ Rails.application.routes.draw do
   get "/delete_photo/:id", :controller => "photos", :action => "destroy"
   #------------------------------
 
+ # Routes for the User resource:
+
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+
+  #------------------------------
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
