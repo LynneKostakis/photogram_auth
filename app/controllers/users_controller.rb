a0f4e9fe0bc7cea748656327094c7faa75bed608
Lynne Kostakis <lkostaki@chicagobooth.edu>
2017-08-10 01:15:52 +0000

    sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,16 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+   
 
     render("users/index.html.erb")
   end
 
   def show
-    @photo = Photo.find(params[:id])
+    @user = User.find(params[:id])
+    @photos = @user.photos
 
-    render("photos/show.html.erb")
+    render("users/show.html.erb")
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -53,15 +53,17 @@ Rails.application.routes.draw do
   get "/delete_photo/:id", :controller => "photos", :action => "destroy"
   #------------------------------
 
- # Routes for the User resource:
+ 
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount WebGit::Engine, at: "/rails/git"
+  
+  # Routes for the User resource:
 
   # READ
   get "/users", :controller => "users", :action => "index"
   get "/users/:id", :controller => "users", :action => "show"
 
   #------------------------------
-
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount WebGit::Engine, at: "/rails/git"
 end
